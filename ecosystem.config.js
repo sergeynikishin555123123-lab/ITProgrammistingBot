6ed2381a6a6b0adb61f77a8112a5bc7fa48388c7
ProgecktProf <sergey.nikishin555123123@gmail.com>
2025-12-01 00:09:54 +0300

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [{
+    name: "codefarm",
+    script: "backend/main.py",
+    interpreter: "python3",
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: "1G",
+    env: {
+      NODE_ENV: "production",
+      PORT: 8000
+    },
+    error_file: "logs/err.log",
+    out_file: "logs/out.log",
+    log_file: "logs/combined.log",
+    time: true
+  }]
+};
