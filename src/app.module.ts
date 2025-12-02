71e96065ae0b19224498a6a0e058e1b8f410fe10
ProgecktProf <sergey.nikishin555123123@gmail.com>
2025-12-02 14:41:14 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,1 +1,30 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { BotModule } from './bot/bot.module';
+import { LessonsModule } from './lessons/lessons.module';
+import { DatabaseModule } from './database/database.module';
+import { WebModule } from './web/web.module';
 
+@Module({
+  imports: [
+    // Загрузка .env файла
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    
+    // Статические файлы
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      serveRoot: '/',
+    }),
+    
+    // Модули приложения
+    BotModule,
+    LessonsModule,
+    DatabaseModule,
+    WebModule,
+  ],
+})
+export class AppModule {}
